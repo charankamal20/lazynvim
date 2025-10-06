0f8f544eb1456130b8d025cad558451a4947ecbd
Charan Kamal Singh <charanksingh20@gmail.com>
2025-10-06 19:48:21 +0530

    Add basic nvim config

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,8 @@
+return {
+  {'neovim/nvim-lspconfig'},             -- LSP configurations
+  {'williamboman/mason.nvim'},           -- Installer for external tools
+  {'williamboman/mason-lspconfig.nvim'}, -- mason extension for lspconfig
+  {'hrsh7th/nvim-cmp'},                  -- Autocomplete engine
+  {'hrsh7th/cmp-nvim-lsp'},              -- Completion source for LSP
+  {'L3MON4D3/LuaSnip'},                  -- Snippet engine
+}
